8e98c0c53af33baa2b8515d8ef47ae9f1f343adf
Tahleel <133563945+Tahleel1611@users.noreply.github.com>
2025-12-04 13:27:32 +0530

    Initialize weather_agent package with metadata

---
+++ weather_agent/__init__.py
@@ -0,0 +1,9 @@
+"""Weather Agent Package
+
+A weather information agent that provides real-time weather data.
+"""
+
+from .agent import WeatherAgent
+
+__all__ = ['WeatherAgent']
+__version__ = '1.0.0'
